ea281e11f67c3e9df2036f81e7129fc966c90aca
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-13 12:17:51 +0900

    ✨ Progress作成

---
+++ src/components/Progress.tsx
@@ -0,0 +1,29 @@
+import type { VFC } from "react";
+import React from "react";
+import { StyleSheet } from "react-native";
+import * as RNProgress from "react-native-progress";
+import { View } from "src/components/custom";
+import { useThemeColor } from "src/hooks/useThemeColor";
+
+export const Progress: VFC = () => {
+	const primary = useThemeColor({}, "primary");
+	return (
+		<View style={defaultStyle.center}>
+			<RNProgress.CircleSnail
+				color={primary}
+				size={60}
+				thickness={4}
+				spinDuration={800}
+			/>
+		</View>
+	);
+};
+
+const defaultStyle = StyleSheet.create({
+	center: {
+		flex: 1,
+		paddingTop: 100,
+		width: "100%",
+		alignItems: "center",
+	},
+});
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,14 @@ import { AuthLayout } from "./AuthLayout";
 import { CircleKeyButton } from "./CircleKeyButton";
 import { ErrorMessage } from "./ErrorMessage";
 import { KeyButton } from "./KeyButton";
+import { Progress } from "./Progress";
+import { Toaster } from "./Toaster";
 
-export { AuthLayout, CircleKeyButton, ErrorMessage, KeyButton };
+export {
+	AuthLayout,
+	CircleKeyButton,
+	ErrorMessage,
+	KeyButton,
+	Progress,
+	Toaster,
+};
