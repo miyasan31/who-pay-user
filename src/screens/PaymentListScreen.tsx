20aa4a89e014674d96cf4c0023bc02881a8518a5
miyasan_0301 <miyasan.19990301@gmail.com>
2021-12-15 10:51:28 +0900

    ✨ PaymentListScreenのfetch→表示

--- src/screens/PaymentListScreen.tsx
+++ src/screens/PaymentListScreen.tsx
@@ -1,25 +1,58 @@
 import type { VFC } from "react";
-import React, { useCallback } from "react";
-import { ColorButton, Text, View } from "src/components/custom";
-import { buttonStyles, textStyles, viewStyles } from "src/styles";
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+import { Progress } from "src/components";
+import { SafeAreaView, Text } from "src/components/custom";
+import { ListView } from "src/components/ListView";
+import { useGetSWRdev } from "src/hooks";
 import type { PaymentScreenProps } from "types";
+import type { Payment } from "types/fetcher";
 
 export const PaymentListScreen: VFC<PaymentScreenProps<"PaymentList">> = (
 	props
 ) => {
-	const onNavigation = useCallback(() => {
-		props.navigation.navigate("PaymentDetail");
-	}, []);
+	const { data, isError, isLoading } = useGetSWRdev<Payment[]>("/payment");
+
+	const renderItem = ({ item }: { item: Payment }) => {
+		const onNavigation = () => {
+			props.navigation.navigate("PaymentDetail", {
+				id: item.id,
+				amount: item.amount,
+				shopName: item.Shop.shopName,
+			});
+		};
+		return (
+			<ListView onPress={onNavigation}>
+				<Text style={styles.title}>{item.amount}</Text>
+				<Text style={styles.title}>{item.Shop.shopName}</Text>
+			</ListView>
+		);
+	};
 
 	return (
-		<View style={viewStyles.full}>
-			<Text style={textStyles.title}>PaymentListScreen</Text>
-
-			<ColorButton
-				title="詳細ページへ"
-				outlineStyle={[buttonStyles.outline, buttonStyles.semi]}
-				onPress={onNavigation}
-			/>
-		</View>
+		<SafeAreaView>
+			{isLoading ? (
+				<Progress />
+			) : isError ? (
+				<Text>エラーが発生しました</Text>
+			) : !data ? (
+				<Text>データがありません</Text>
+			) : data ? (
+				<FlatList
+					data={data}
+					renderItem={renderItem}
+					keyExtractor={(item, _) => String(item.id)}
+				/>
+			) : null}
+		</SafeAreaView>
 	);
 };
+
+const styles = StyleSheet.create({
+	title: {
+		fontSize: 15,
+		fontWeight: "normal",
+		textAlign: "left",
+		color: "#010101",
+	},
+});
