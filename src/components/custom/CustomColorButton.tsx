af6ff0a27777b6649336f7b9af8601769ad4d514
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-08 16:52:13 +0900

    💄 Button

--- src/components/custom/CustomColorButton.tsx
+++ src/components/custom/CustomColorButton.tsx
@@ -1,8 +1,8 @@
 import type { VFC } from "react";
 import React, { memo } from "react";
 import {
-  StyleSheet,
-  TouchableOpacity as NativeTouchableOpacity,
+	StyleSheet,
+	TouchableOpacity as NativeTouchableOpacity,
 } from "react-native";
 import type { TextProps } from "src/components/custom/CustomText";
 import { CustomText as Text } from "src/components/custom/CustomText";
@@ -11,77 +11,77 @@ import { useThemeColor } from "src/hooks";
 import type { StyleProps } from "types/style";
 
 export type ColorButtonProps = StyleProps &
-  TextProps &
-  NativeTouchableOpacity["props"] & {
-    title?: string;
-  };
+	TextProps &
+	NativeTouchableOpacity["props"] & {
+		title?: string;
+	};
 
 export const CustomColorButton: VFC<ColorButtonProps> = memo((props) => {
-  const {
-    // TextProps
-    title,
-    children,
-    textStyle,
-    lightTextColor,
-    darkTextColor,
-    // TouchableOpacityProps
-    bgStyle,
-    lightBgColor,
-    darkBgColor,
-    // ViewProps
-    outlineStyle,
-    onPress,
-  } = props;
+	const {
+		// TextProps
+		title,
+		children,
+		textStyle,
+		lightTextColor,
+		darkTextColor,
+		// TouchableOpacityProps
+		bgStyle,
+		lightBgColor,
+		darkBgColor,
+		// ViewProps
+		outlineStyle,
+		onPress,
+	} = props;
 
-  const backgroundColor = useThemeColor(
-    { light: lightBgColor, dark: darkBgColor },
-    "primary"
-  );
-  const color = useThemeColor(
-    { light: lightTextColor, dark: darkTextColor },
-    "text3"
-  );
+	const backgroundColor = useThemeColor(
+		{ light: lightBgColor, dark: darkBgColor },
+		"primary"
+	);
+	const color = useThemeColor(
+		{ light: lightTextColor, dark: darkTextColor },
+		"text3"
+	);
 
-  return (
-    <View style={[defaultStyles.outline, outlineStyle]}>
-      <NativeTouchableOpacity
-        style={[defaultStyles.bg, bgStyle, { backgroundColor }]}
-        activeOpacity={0.4}
-        onPress={onPress}
-      >
-        {children}
+	return (
+		<View style={[defaultStyles.outline, outlineStyle]}>
+			<NativeTouchableOpacity
+				style={[defaultStyles.bg, bgStyle, { backgroundColor }]}
+				activeOpacity={0.4}
+				onPress={onPress}
+			>
+				{children}
 
-        {title ? (
-          <Text
-            lightTextColor={color}
-            darkTextColor={color}
-            style={[defaultStyles.text, textStyle]}
-          >
-            {title}
-          </Text>
-        ) : null}
-      </NativeTouchableOpacity>
-    </View>
-  );
+				{title ? (
+					<Text
+						lightTextColor={color}
+						darkTextColor={color}
+						style={[defaultStyles.text, textStyle]}
+					>
+						{title}
+					</Text>
+				) : null}
+			</NativeTouchableOpacity>
+		</View>
+	);
 });
 
 const defaultStyles = StyleSheet.create({
-  outline: {
-    width: "100%",
-    borderRadius: 9999,
-  },
-  bg: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+	outline: {
+		width: "100%",
+		borderRadius: 9999,
+	},
+	bg: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
 
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    borderRadius: 9999,
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
+		paddingVertical: 18,
+		paddingHorizontal: 15,
+		borderRadius: 9999,
+	},
+	text: {
+		fontSize: 20,
+		fontWeight: "bold",
+		textAlign: "center",
+	},
 });
