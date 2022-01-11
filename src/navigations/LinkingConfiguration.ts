6b5005c81a1be403fd130023d359713c7af65fc5
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-11 13:49:55 +0900

    🚧 型エラー修正途中

--- src/navigations/LinkingConfiguration.ts
+++ src/navigations/LinkingConfiguration.ts
@@ -26,18 +26,89 @@ export const LinkingConfiguration: LinkingOptions<RootStackParamList> = {
           },
         },
       },
-      // Pay: {
-      // 	path: "pay",
-      // 	screens: {
-      // 		VoiceRecord: {
-      // 			path: "voice_record/:price",
-      // 		},
-      // 		Passcode: {
-      // 			path: "passcode/:price",
-      // 		},
-      // 	},
-      // },
+      Main: {
+        path: "main",
+        screens: {
+          Top: {
+            path: "top",
+            // screens: {
+            //   Home: {
+            //     path: "home",
+            //   },
+            // },
+          },
+          Payment: {
+            path: "payment",
+            // screens: {
+            //   PaymentList: {
+            //     path: "payment_list",
+            //   },
+            //   PaymentDetail: {
+            //     path: "payment_detail/:id",
+            //   },
+            // },
+          },
+          Setting: {
+            path: "setting",
+            //   screens: {
+            //     SettingSelect: {
+            //       path: "setting_select",
+            //     },
+            //     AccountSetting: {
+            //       path: "account_setting",
+            //       screens: {
+            //         Account: {
+            //           path: "account",
+            //         },
+            //         AccountUpdate: {
+            //           path: "account_update",
+            //         },
+            //       }
+            //     },
+            //     CreditSetting: {
+            //       path: "credit_setting",
+            //       screens: {
+            //         Credit: {
+            //           path: "credit",
+            //         },
+            //         CreditUpdate: {
+            //           path: "credit_update",
+            //         },
+            //     },
+            //     PasscodeSetting: {
+            //       path: "passcode_setting",
+            //       screens: {
+            //         PasscodeSettingSelect: {
+            //           path: "passcode_setting_select",
+            //         },
+            //         Passcode: {
+            //           path: "passcode",
+            //         },
+            //         PasscodeUpdate: {
+            //           path: "passcode_update",
+            //         },
 
+            //       }
+            //     },
+            //     VoiceRecordSetting: {
+            //       path: "voice_record_setting",
+            //       screens: {
+            //         VoiceRecordSettingSelect: {
+            //           path: "voice_record_setting_select",
+            //         },
+            //         VoiceRecord: {
+            //           path: "voice_record",
+            //         },
+            //         VoiceRecordUpdate: {
+            //           path: "voice_record_update",
+            //         },
+
+            //       }
+            //     },
+            //   },
+          },
+        },
+      },
       Modal: "modal",
       NotFound: "*",
     },
