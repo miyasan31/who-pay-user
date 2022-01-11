def13d61015fab84fc7399c241a48dec788cfa95
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-11 13:44:40 +0900

    🔥 コメントアウト削除

--- src/navigations/Navigator.tsx
+++ src/navigations/Navigator.tsx
@@ -1,11 +1,9 @@
-// import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import { useThemeColor } from "src/hooks";
 import { MainBottomTabNavigator } from "src/screens/main";
 import type { RootStackParamList } from "types";
 
-// const Drawer = createDrawerNavigator<RootStackParamList>();
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export const Navigator = () => {
