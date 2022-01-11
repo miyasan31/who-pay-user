deb4e69c38166d91843ba13a921981fe3f1fb4a2
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-11 13:46:41 +0900

    ♻️ memo化を修正

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -4,16 +4,14 @@ import {
   NavigationContainer,
 } from "@react-navigation/native";
 import type { VFC } from "react";
-import React, { useMemo } from "react";
+import React from "react";
 import type { ColorSchemeName } from "react-native";
 import { AuthProvider } from "src/navigations/AuthProvider";
 import { LinkingConfiguration } from "src/navigations/LinkingConfiguration";
 import { Navigator } from "src/navigations/Navigator";
 
 export const Navigations: VFC<{ colorScheme: ColorSchemeName }> = (props) => {
-  const themeResult = useMemo(() => {
-    return props?.colorScheme === "dark" ? DarkTheme : DefaultTheme;
-  }, [props]);
+  const themeResult = props?.colorScheme === "dark" ? DarkTheme : DefaultTheme;
 
   return (
     <NavigationContainer linking={LinkingConfiguration} theme={themeResult}>
